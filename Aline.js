cb8eef42eb1a8d74376b89ae7d9210b3634573d4
Aline Barros <73312699+alinebarros12@users.noreply.github.com>
2022-10-20 20:22:46 -0300

    Create Aline.js

---
+++ Aline.js
@@ -0,0 +1,16 @@
+function alert (){
+
+alert()
+
+}
+// Add your code here
+
+let myName = 'Paul';
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+const para = document.createElement('p');
+para.textContent = myName;
+section.appendChild(para);
+    
